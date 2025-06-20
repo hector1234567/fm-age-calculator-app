dd7c5c85de4a7db110bb36f8154bd76666ac92be
Hector B <hectorbg303@gmail.com>
2025-06-20 11:43:31 +0200

    Calculate

--- utils.js
+++ utils.js
@@ -1,11 +1,26 @@
+import {
+  differenceInYears,
+  differenceInMonths,
+  differenceInDays,
+  addYears,
+  addMonths,
+} from "date-fns";
+
 export function getDaysMonthsAndYearsFromBirthdate(
   birthDay,
   birthMonth,
   birthYear
 ) {
-  return {
-    days: 1,
-    months: 2,
-    years: 3,
-  };
+  const now = new Date();
+  const birth = new Date(birthYear, birthMonth, birthDay);
+
+  const years = differenceInYears(now, birth);
+  const dateAfterYears = addYears(birth, years);
+
+  const months = differenceInMonths(now, dateAfterYears);
+  const dateAfterMonths = addMonths(dateAfterYears, months);
+
+  const days = differenceInDays(now, dateAfterMonths);
+
+  return { days, months, years };
 }
